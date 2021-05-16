8e85949a795c4aa716ac23413894a9704610973c
GaoLiHai <1019933576@qq.com>
2021-05-16 18:47:45 +0800

    refactor: 删掉无用的测试代码

--- core/analysisNOLevel.js
+++ core/analysisNOLevel.js
@@ -7,28 +7,3 @@ module.exports = async ({ minlat, maxlat, minlon, maxlon }, year, month) => {
   );
   return JSON.parse(stdout);
 }
-
-// let st = Date.now();
-// fun({
-//   minlat: 36.5,
-//   maxlat: 38.5,
-//   minlon: 119.9,
-//   maxlon: 121.9,
-// }, 2019, 1).then((json) => {
-//   console.log(Date.now() - st);
-//   console.log(json);
-//   require('fs').writeFileSync('./a.jpg', Buffer.from(json.imgBase64, 'base64'));
-// });
-
-// (async () => {
-//   try {
-//     var { stdout } = await execa(
-//       `python b:\\workspace\\Git\\no-web\\core\\NOLevelPy\\HLWP1_main.py --minlat 36.563808 --maxlat 36.664154 --minlon 114.402726 --maxlon 114.579512 --year 2019 --month 1`
-//     );
-//   } catch (error) {
-//     console.log(error);
-
-//   }
-
-//   require('fs').writeFileSync('./a.jpg', Buffer.from(JSON.parse(stdout).imgBase64), 'base64');
-// })()
